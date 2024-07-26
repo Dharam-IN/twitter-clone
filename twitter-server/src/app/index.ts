9bbc6bc68170c007a2c0f0eadd6da712202003c7
dharam-in <dharamdotin@gmail.com>
2024-07-26 20:58:44 +0530

    Prisma User Added

---
+++ twitter-server/src/app/index.ts
@@ -0,0 +1,31 @@
+import {ApolloServer} from '@apollo/server';
+import express from 'express';
+import {expressMiddleware} from '@apollo/server/express4';
+import bodyParser from 'body-parser';
+import { prismaClient } from '../clients/db';
+
+
+export async function initServer(){
+    const app = express();
+
+    app.use(bodyParser.json());
+
+    const graphqlServer = new ApolloServer({
+        typeDefs: `
+            type Query {
+                sayHello: String
+            }
+        `,
+        resolvers: {
+            Query: {
+                sayHello: () => 'Hello from graphql Server'
+            },
+        }
+    });
+
+    await graphqlServer.start();
+
+    app.use("/graphql", expressMiddleware(graphqlServer))
+
+    return app;
+}
